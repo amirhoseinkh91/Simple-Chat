e7a77026c367c87561efec1b72b556c9fd82f941
AmirHoseinKhalouei <a.khalouei@outlook.com>
2017-04-18 14:11:52 +0430

    clinet class and user class added

---
+++ module/src/main/java/client/Client.java
@@ -0,0 +1,26 @@
+package client;
+
+import java.net.Socket;
+import java.util.Formatter;
+import java.util.Scanner;
+
+/**
+ * Created by amir on 4/18/17.
+ */
+public class Client {
+
+    String next;
+    private Socket socket;
+    private Scanner input;
+    private Formatter output;
+
+    public void start (){
+        try{
+
+        } catch (Exception e){
+
+        } finally {
+
+        }
+    }
+}
---
+++ module/src/main/java/user/User.java
@@ -0,0 +1,8 @@
+package user;
+
+/**
+ * Created by amir on 4/18/17.
+ */
+public class User {
+
+}
