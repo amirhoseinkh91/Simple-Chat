89ada75c79ff6c77f7205a3d62c3358c636151e7
Eric_DA <manutd5redarmy@gmail.com>
2017-04-18 16:39:15 +0430

    some changes in client handler

--- module/src/main/java/server/ClientHandler.java
+++ module/src/main/java/server/ClientHandler.java
@@ -1,6 +1,7 @@
 package server;
 
 
+import client.Client;
 import user.User;
 
 import java.io.*;
@@ -8,18 +9,20 @@ import java.net.ServerSocket;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.HashSet;
 
 /**
  * Created by eric on 4/18/17.
  */
 public class ClientHandler extends Thread {
-    private User currentUser;
+    private Client currentUser;
+    private static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
     private Socket currentSocket;
     private BufferedReader in;
     private PrintWriter out;
     Server server = new Server();
 
-    public ClientHandler(User currentUser) {
+    public ClientHandler(Client currentUser) {
         this.currentUser = currentUser;
     }
 
@@ -40,22 +43,42 @@ public class ClientHandler extends Thread {
             while (true) {
                 out.println("username");
                 try {
-                    String user = currentUser.getUsername();
+                    String user = currentUser.getUser().getUsername();
                     user = in.readLine();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                if (currentUser == null || currentUser.getUsername().trim().length() == 0) {
+
+                if (currentUser.getUser().getUsername() == null ||
+                        currentUser.getUser().getUsername().trim().length() == 0) {
                     return;
                 }
                 synchronized (server.getUsers()) {
-                    if (!server.getUsers().contains(currentUser)) {
-                        server.getUsers().add(currentUser);
+                    if (!server.getUsers().contains(currentUser.getUser())) {
+                        server.getUsers().add(currentUser.getUser());
                         break;
                     }
                 }
 
             }
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+                if (currentSocket != null) {
+                    server.getUsers().remove(currentSocket);
+
+                }
+                if (out != null) {
+                    writers.remove(out);
+                }
+                try {
+                    currentSocket.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
         }
     }
 }
