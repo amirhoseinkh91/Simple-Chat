f937576c0027834f1aaf512656557d51fbda37f1
AmirHoseinKhalouei <a.khalouei@outlook.com>
2017-04-18 14:40:24 +0430

    client class fields generated

--- module/src/main/java/client/Client.java
+++ module/src/main/java/client/Client.java
@@ -1,5 +1,6 @@
 package client;
 
+import java.io.IOException;
 import java.net.Socket;
 import java.util.Formatter;
 import java.util.Scanner;
@@ -13,14 +14,93 @@ public class Client {
     private Socket socket;
     private Scanner input;
     private Formatter output;
+    private String serverIp;
+    private int serverPort = 8090;
+
+    // constructor
+
+
+    public Client(String serverIp, int serverPort) {
+        this.serverIp = serverIp;
+        this.serverPort = serverPort;
+    }
+
+    // setter getters
+    public String getNext() {
+        return next;
+    }
+
+    public void setNext(String next) {
+        this.next = next;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+
+    public Scanner getInput() {
+        return input;
+    }
+
+    public void setInput(Scanner input) {
+        this.input = input;
+    }
+
+    public Formatter getOutput() {
+        return output;
+    }
+
+    public void setOutput(Formatter output) {
+        this.output = output;
+    }
+
+    public String getServerIp() {
+        return serverIp;
+    }
+
+    public void setServerIp(String serverIp) {
+        this.serverIp = serverIp;
+    }
+
+    public int getServerPort() {
+        return serverPort;
+    }
+
+    public void setServerPort(int serverPort) {
+        this.serverPort = serverPort;
+    }
 
     public void start (){
-        try{
+        try {
+            socket = new Socket(serverIp,serverPort);
+            input = new Scanner(socket.getInputStream());
+            output = new Formatter(socket.getOutputStream());
+            String recieved;
+            do {
+                next = input.next();
+                output.format(next + "\n");
+                // show output on my device
+                // showOutput
+                output.flush();
+                recieved = input.next();
+                // shoe input
+                // showInput
 
-        } catch (Exception e){
-
+            } while (!recieved.equals("exit"));
+        } catch (IOException e){
+            e.printStackTrace();
         } finally {
-
+            output.close();
+            input.close();
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
